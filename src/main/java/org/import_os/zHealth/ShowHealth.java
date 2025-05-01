bd0504f52cc6143de913d8d59aba2420765d3ad8
import0s <gamerboyita937@gmail.com>
2025-05-01 22:46:15 +0200

    Created the commands /showhealth and /showhunger

---
+++ src/main/java/org/import_os/zHealth/ShowHealth.java
@@ -0,0 +1,26 @@
+package org.import_os.zHealth;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ShowHealth implements CommandExecutor {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = Bukkit.getPlayer(args[0]);
+        if (args[0].isEmpty()) {
+            sender.sendMessage(ChatColor.RED + "Usage: /showhealth <player>");
+            return true;
+        }
+        if (player == null) {
+            sender.sendMessage(ChatColor.RED + "Player not found");
+            return true;
+        }
+
+        sender.sendMessage(ChatColor.GREEN + "The health of " + player.getName() + " is " + player.getHealth());
+
+        return true;
+    }
+}
---
+++ src/main/java/org/import_os/zHealth/ShowHunger.java
@@ -0,0 +1,26 @@
+package org.import_os.zHealth;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ShowHunger implements CommandExecutor {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = Bukkit.getPlayer(args[0]);
+        if (args[0].isEmpty()) {
+            sender.sendMessage(ChatColor.RED + "Usage: /showhealth <player>");
+            return true;
+        }
+        if (player == null) {
+            sender.sendMessage(ChatColor.RED + "Player not found");
+            return true;
+        }
+
+        sender.sendMessage(ChatColor.GREEN + "The Food level of " + player.getName() + " is " + player.getFoodLevel());
+
+        return true;
+    }
+}
