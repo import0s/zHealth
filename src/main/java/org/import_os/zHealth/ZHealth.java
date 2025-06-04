937b6b3fefe1c08d5f161ad7a5b05b2c4bd860d3
import0s <gamerboyita937@gmail.com>
2025-06-04 14:34:07 +0200

    implemented an update checker for the future versions

--- src/main/java/org/import_os/zHealth/ZHealth.java
+++ src/main/java/org/import_os/zHealth/ZHealth.java
@@ -2,11 +2,29 @@ package org.import_os.zHealth;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public final class ZHealth extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        String currentVer =  getDescription().getVersion();
+        String versionURL = "https://raw.githubusercontent.com/import0s/zHealth/master/src/main/resources/version.txt";
+
+        try {
+            BufferedReader reader =  new BufferedReader(new InputStreamReader(new URL(versionURL).openStream()));
+            String gitVer = reader.readLine().trim();
+            reader.close();
+            if (!gitVer.equals(currentVer)) {
+                getLogger().warning("The plugin is OUTDATED! Download the latest version on GitHub");
+            }
+        } catch (Exception e) {
+            getLogger().severe("An error has occurred during the Update check: " + e.getMessage());
+        }
 
         long start = System.nanoTime();
         getLogger().info("zHealth Enabled v1.0");
